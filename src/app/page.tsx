512e64184e6f4f5c98afc94afcb5f90c841f6076
Daniel <zerpadaniel2005@gmail.com>
2026-04-13 22:50:12 -0300

    Remove unused SVG files and add new dashboard and materias pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="h-screen flex justify-center items-center p-5 bg-linear-to-r from-rose-100 to-teal-100">
+    <main className="h-screen flex justify-center items-center bg-linear-to-r from-rose-100 to-teal-100">
       <section className="bg-foreground rounded-lg p-5 ">
         <h1 className="font-heading text-4xl text-text-1 font-bold">Hola &</h1>
         <h2 className="text-2xl text-text-2 font-medium">Esto es una prueba</h2>
