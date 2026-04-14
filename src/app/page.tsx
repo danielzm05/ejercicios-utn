678d5ce7796e0ede86296e6050568e481a6fda79
Daniel <zerpadaniel2005@gmail.com>
2026-04-13 21:54:07 -0300

    Refactor styles and fonts for improved UI consistency and readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="h-screen flex justify-center items-center p-5">
-      <section className="bg-foreground rounded-2xl p-5">
-        <h1 className="font-mono text-4xl text-text-1 font-bold">Hola :)</h1>
-        <h2 className="font-sans text-2xl text-text-2 font-medium">Esto es una prueba</h2>
-        <p className="font-sans text-text-3 font-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur reiciendis culpa nihil eius, assumenda cum sequi optio earum quibusdam eveniet! Quo odio inventore quam fuga numquam possimus iusto quidem molestiae.</p>
+    <main className="h-screen flex justify-center items-center p-5 bg-linear-to-r from-rose-100 to-teal-100">
+      <section className="bg-foreground rounded-lg p-5 ">
+        <h1 className="font-heading text-4xl text-text-1 font-bold">Hola &</h1>
+        <h2 className="text-2xl text-text-2 font-medium">Esto es una prueba</h2>
+        <p className="font-p text-text-3 font-normal">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur reiciendis culpa nihil eius, assumenda cum sequi optio earum quibusdam
+          eveniet! Quo odio inventore quam fuga numquam possimus iusto quidem molestiae. &
+        </p>
       </section>
     </main>
   );
